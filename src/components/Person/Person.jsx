fd71b74a95663b5d7c82de4fa5769ab24e613b33
Rostuk Dudchak <rostikdudchak15@gmail.com>
2024-04-15 15:16:21 +0300

    person finish

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,31 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">
+        {(() => {
+          if (isMarried) {
+            if (sex === 'm') {
+              return `${partnerName} is my wife`;
+            }
+
+            if (sex === 'f') {
+              return `${partnerName} is my husband`;
+            }
+          }
+
+          return 'I am not married';
+        })()}
+
+        {/* {isMarried
+          ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+          : 'l am not married'} */}
+      </p>
+    </section>
+  );
+};
